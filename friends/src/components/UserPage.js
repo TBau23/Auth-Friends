15f8b105ec933e73d0eb8f30acf54d56e0083c99
Tom Bauer <tbauer1898@gmail.com>
2020-09-15 23:08:02 -0500

    Done with MVP.

--- friends/src/components/UserPage.js
+++ friends/src/components/UserPage.js
@@ -6,7 +6,13 @@ import AddFriend from './AddFriend'
 class UserPage extends React.Component  {
 
     state = {
-        friendsList: []
+        friendsList: [],
+        friend: {
+            id: new Date(),
+            name: "",
+            age: "",
+            email: ""
+        }
     }
 
     componentDidMount() {
@@ -28,11 +34,69 @@ class UserPage extends React.Component  {
         })
     }
 
+    postFriend = e => {
+        e.preventDefault()
+
+        axiosWithAuth()
+            .post("/api/friends", this.state.friend)
+            .then(res => {
+                this.setState({
+                    friendsList: res.data
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    handleChange = e => {
+        this.setState({
+            friend: {
+                ...this.state.friend,
+                [e.target.name] : e.target.value
+            }
+        })
+    }
+
 render() {
     return(
         <div>
             <h2>Authenticated Users Only</h2>
-            <AddFriend />
+            <form className='add-friend' onSubmit={this.postFriend}>
+                <h2>Add a Friend</h2>
+                <label>Name
+                    <input
+                    type='text'
+                    name='name'
+                    placeholder="enter friend's name"
+                    value={this.state.friend.name}
+                    onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>Age
+                    <input
+                    type='text'
+                    name='age'
+                    placeholder="enter friend's age"
+                    value={this.state.friend.age}
+                    onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>Email
+                    <input
+                    type='email'
+                    name='email'
+                    placeholder="enter friend's age"
+                    value={this.state.friend.email}
+                    onChange={this.handleChange}
+                    />
+                </label>
+
+                <button>Click to Add Friend</button>
+
+            </form>
             <div>
                 <h2>Your friends:</h2>
                 {this.state.friendsList.map(friend => (
